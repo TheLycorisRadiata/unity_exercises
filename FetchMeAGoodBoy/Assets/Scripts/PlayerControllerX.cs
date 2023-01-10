f35eb4d1c21f68a57aae4fc5e2b277f454f572b3
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-01-10 14:47:05 +0100

    FetchMeAGoodBoy: [Bonus] Player can no longer spam, they have to wait a second before they can spawn another dog

--- FetchMeAGoodBoy/Assets/Scripts/PlayerControllerX.cs
+++ FetchMeAGoodBoy/Assets/Scripts/PlayerControllerX.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool canSpawnDog;
+
+    void Start()
+    {
+        canSpawnDog = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (canSpawnDog && Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            StartCoroutine(SpawnDog());
         }
+    }
+
+    IEnumerator SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        canSpawnDog = false;
+        yield return new WaitForSeconds(1);
+        canSpawnDog = true;
     }
 }
