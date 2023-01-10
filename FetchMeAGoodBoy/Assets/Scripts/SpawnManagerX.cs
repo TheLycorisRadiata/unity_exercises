1a985f5da2ad628cc22d39b0ec1b08b5497d6ae8
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2023-01-10 14:30:50 +0100

    FetchMeAGoodBoy: [Bonus] Random time interval (between 3 and 5s) for ball instantiation

--- FetchMeAGoodBoy/Assets/Scripts/SpawnManagerX.cs
+++ FetchMeAGoodBoy/Assets/Scripts/SpawnManagerX.cs
@@ -11,12 +11,13 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnIntervalMin = 3.0f;
+    private float spawnIntervalMax = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(spawnIntervalMin, spawnIntervalMax));
     }
 
     // Spawn random ball at random x position at top of play area
